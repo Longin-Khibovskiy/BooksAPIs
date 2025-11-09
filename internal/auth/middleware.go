be8a408f8e2f6e0ab920eb04dd0ae14596b3f7c9
Longin <l.khibovskiy@gmail.com>
2025-11-10 01:20:29 +0300

    add middleware

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -1,1 +1,36 @@
 package auth
+
+import (
+	"context"
+	"net/http"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secret = []byte(os.Getenv("JWT_SECRET"))
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("auth_token")
+		if err != nil {
+			http.Error(w, "Authorization required", http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := cookie.Value
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			return secret, nil
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		userID := int(claims["sub"].(float64))
+		ctx := context.WithValue(r.Context(), "userID", userID)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
